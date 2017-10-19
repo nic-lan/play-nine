d7a7eb49dd75c4df038fcba4a66db563c3e52268
nic-lan <nicolas.languille@gmail.com>
2017-10-19 18:47:21 +0200

    Add real bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import Stars from './Stars';
+import { Grid, Col, Row } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Stars starsNumber={5} />
+        <Grid>
+          <Row>
+            <h2>Play Nine</h2>
+          </Row>
+          <Row>
+            <Col xs={5}>
+              <Stars starsNumber={5} />
+            </Col>
+            <Col xs={2}>
+              =
+            </Col>
+            <Col xs={5}>
+              Asnwer
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
