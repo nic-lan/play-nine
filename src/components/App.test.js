563d53e084b6cfff81216fd33f61178206a39872
nic-lan <nicolas.languille@gmail.com>
2017-10-21 17:22:10 +0200

    Add initial app sstate

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -12,8 +12,23 @@ describe('App', () => {
 
   it('renders without crashing', () => {
     const wrapper = mount(<App />)
-    expect(wrapper.state('numbers')).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    expect(wrapper.state('starsNumber')).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    expect(wrapper.state('selectedNumbers')).toEqual([1, 2, 3, 4, 5])
+
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(wrapper.state('numbers')).toEqual(numbers)
+
+    const starsNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(wrapper.state('starsNumber')).toEqual(starsNumber)
+
+    const selectedNumbers = [1, 2, 3, 4, 5]
+    expect(wrapper.state('selectedNumbers')).toEqual(selectedNumbers)
+
+    // check initial number of rendered stars
+    expect(wrapper.find('Star').length).toEqual(starsNumber.length)
+
+    // check initial rendered Numbers
+    expect(wrapper.find('Numbers').find('Number').length).toEqual(numbers.length)
+
+    // check initial rendered SelectedNumbers
+    expect(wrapper.find('SelectedNumbers').find('Number').length).toEqual(selectedNumbers.length)
   })
 })
