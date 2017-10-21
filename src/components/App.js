9db509e66964ffcf29499ead1eef42d8babb1455
nic-lan <nicolas.languille@gmail.com>
2017-10-21 17:41:34 +0200

    Set initial state of selected number to 0

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     this.state = {
       numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
       starsNumber: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      selectedNumbers: [1, 2, 3, 4, 5]
+      selectedNumbers: []
     }
   }
   render() {
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -19,16 +19,15 @@ describe('App', () => {
     const starsNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     expect(wrapper.state('starsNumber')).toEqual(starsNumber)
 
-    const selectedNumbers = [1, 2, 3, 4, 5]
-    expect(wrapper.state('selectedNumbers')).toEqual(selectedNumbers)
+    expect(wrapper.state('selectedNumbers')).toHaveLength(0)
 
     // check initial number of rendered stars
-    expect(wrapper.find('Star').length).toEqual(starsNumber.length)
+    expect(wrapper.find('Star')).toHaveLength(starsNumber.length)
 
     // check initial rendered Numbers
-    expect(wrapper.find('Numbers').find('Number').length).toEqual(numbers.length)
+    expect(wrapper.find('Numbers').find('Number')).toHaveLength(9)
 
     // check initial rendered SelectedNumbers
-    expect(wrapper.find('SelectedNumbers').find('Number').length).toEqual(selectedNumbers.length)
+    expect(wrapper.find('SelectedNumbers').find('Number')).toHaveLength(0)
   })
 })
